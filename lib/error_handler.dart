eb31bd4bdf135885bc03fd953c841ce1c0dd2a69
M Atheer <55885230+masreplay@users.noreply.github.com>
2022-08-21 21:28:51 +0300

    fix: urls

--- lib/error_handler.dart
+++ lib/error_handler.dart
@@ -1,11 +1,7 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+/// Error handler And api request state handling
 library error_handler;
 
 export 'error_handler_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
 
 export 'src/https_response.dart';
 export 'src/network_exception.dart';
